0e79ae195b207415004e5a6275e1ae804f5a6b3a
Fiona Voss <me@fionavoss.com>
2024-11-27 19:02:54 -0800

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Fiona Voss on 11/27/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RockPaperScissorsTests/RockPaperScissorsTests.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsTests.swift
+//  RockPaperScissorsTests
+//
+//  Created by Fiona Voss on 11/27/24.
+//
+
+import Testing
+@testable import RockPaperScissors
+
+struct RockPaperScissorsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
